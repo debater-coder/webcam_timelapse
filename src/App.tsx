b11fcb6c1170b35cdb463c0182540f134615a98b
Hamzah Ahmed <hamzah@syedahmed.net>
2022-10-12 13:13:58 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import {
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Moon, Sun, Webcam } from "phosphor-react";
+import "@fontsource/poppins";
+
+function App() {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <Flex
+      w="full"
+      p={4}
+      align="center"
+      gap={4}
+      bg={useColorModeValue("gray.100", "gray.700")}
+      borderBottom={`1px solid ${useColorModeValue("black", "white")}`}
+    >
+      <Webcam size={32} weight="duotone" />
+      <Heading size="md" fontFamily={"Poppins"}>
+        WebcamTimelapse
+      </Heading>
+      <Spacer />
+      <IconButton
+        aria-label="toggle dark mode"
+        variant="solid"
+        colorScheme={"teal"}
+        icon={colorMode === "light" ? <Moon /> : <Sun />}
+        onClick={toggleColorMode}
+      />
+    </Flex>
+  );
+}
+
+export default App;
